4f62b4fd8de5bdb3dede9d2485965b810a7c3c1b
Oliver Fleckenstein <oliverfl@live.dk>
2017-02-01 01:57:03 +0100

    Start up on writing an C# API to interface with the Web API of Toggl

---
+++ TogglConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TogglAPI;
+
+namespace TogglConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test().Wait();
+        }
+
+        public static async Task Test()
+        {
+            Authentication au = new Authentication();
+            string result = await au.Logon();
+            Console.WriteLine(result);
+
+            Console.WriteLine("Program done");
+            Console.ReadLine();
+        }
+
+        public static async Task CallRecipeServiceAsync()
+        {
+            var recipes = await new Authentication().GetRecipeDataItemsAsync();
+            foreach (var receipe in recipes)
+            {
+                Console.WriteLine(receipe.Title);
+            }
+        }
+    }
+}
